988012b78400333285a733b27e2b643402697a45
Martijn Lodder <58008304+martijnlodder@users.noreply.github.com>
2023-05-16 20:56:01 +0200

    Delete MartySecure directory

--- MartySecure/Scanner/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class MFASetupForm(forms.Form):
-    mfa_secret = forms.CharField(label='MFA-secret', max_length=16, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
--- MartySecure/Scanner/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ValidatedResult
-
-class ValidatedResultSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ValidatedResult
-        fields = ['port', 'service', 'version', 'vulnerability', 'description', 'severity']
