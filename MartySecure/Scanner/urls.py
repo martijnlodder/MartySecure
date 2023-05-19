afdbbc1031fdf55fc4d79d5852286f3264c200e0
Martijn Lodder <58008304+martijnlodder@users.noreply.github.com>
2023-05-19 17:34:27 +0200

    Delete MartySecure directory

--- MartySecure/Scanner/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import ValidatedResultList
-
-urlpatterns = [
-    path('port_scan/', views.port_scan, name='port_scan'),
-    path('api/results/', ValidatedResultList.as_view(),
-         name='validated_result_list'),
-]
